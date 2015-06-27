9acc708e1f2ecb09a0dcdee7d456ac10151d4402
www.meigui.com1 <codreams@163.com>
2015-06-27 18:02:23 +0800

    1

---
+++ JFinal_Demo/src/demo/controller/HelloDAO.java
@@ -0,0 +1,17 @@
+package demo.controller;
+
+import com.jfinal.plugin.activerecord.Model;
+
+public class HelloDAO extends Model<HelloDAO>{
+	
+	
+	private static final HelloDAO h = new HelloDAO();
+	
+	
+	public static void main(String[] args) {
+		
+		
+	}
+	
+
+}
