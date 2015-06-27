250ec17cf709d14fb75d8fc479c6a7cfbb6eef6e
www.meigui.com1 <codreams@163.com>
2015-06-27 17:56:01 +0800

    1

---
+++ JFinal_Demo/src/demo/AdminController.java
@@ -0,0 +1,12 @@
+package demo;
+
+import com.jfinal.core.Controller;
+
+public class AdminController extends Controller{
+
+	public void index(){
+		
+		renderText("Admini Controller Xavier");
+	}
+	
+}
